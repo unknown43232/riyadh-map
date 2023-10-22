f814ad77bf415cd616568d287c19b8fa3bd094f4
MohammedAldalooj <96469073+mohammedAldalooj-penny@users.noreply.github.com>
2023-10-22 12:33:36 +0300

    test no css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
-import React, { useEffect } from "react"; // <-- Add this import
 import "@/styles/globals.css";
+// import "leaflet/dist/leaflet.css";
 
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    // Dynamically import the Leaflet CSS on the client side
-    import("leaflet/dist/leaflet.css");
-  }, []);
-
   return <Component {...pageProps} />;
 }
